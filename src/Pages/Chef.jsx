f68b979ee3e7d3dfc31b70326c94d6e01a1c000d
unknown <semel.yakoub@gmail.com>
2025-01-13 23:59:07 +0300

    add Quality component with responsive and animations

--- src/Pages/Chef.jsx
+++ src/Pages/Chef.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Background from '../Component/Background/Background'
+import Quality from '../Component/Quality/Quality'
 
 const Chef = () => {
   return (
     <div>
         <Background text ="our chef"/>
+        <Quality/>
         
         
     </div>
